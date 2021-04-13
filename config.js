00d3bddf0436b5c9a59257d7a3f3ea3ebaa43dfe
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-13 17:31:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'DarkZE\n     🔥Nuebalobot🔥'
-global.author = '🔐DarkZE👑'
+global.packname = 'DarkZE\n     🔥DarkZE/BOT🔥'
+global.author = '🔐wa.me/+524447000553👑'
 
 global.multiplier = 69 // The higher, The harder levelup
 
