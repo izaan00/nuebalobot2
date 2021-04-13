d2046b9862a65e6ccc0f115fca17706dcc9d295f
DarkZEKI <81875205+DarkZEKI@users.noreply.github.com>
2021-04-13 14:23:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 global.owner = ['5521970019765'] // Put your number here
-global.mods = [34671350075] // Want some help?
+global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
